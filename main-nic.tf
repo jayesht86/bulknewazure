759ca5f6b4550750152b11deba1262cdbe1ea68c
jayesht86 <37490933+jayesht86@users.noreply.github.com>
2025-03-03 23:30:23 -0800

    Update main-nic.tf

--- main-nic.tf
+++ main-nic.tf
@@ -1,18 +1,18 @@
 
 resource "azurerm_network_interface" "nic" {
-  for_each = { for nic in var.network_interfaces : nic.name => nic }
+  for_each = { for nic in var.network_interfaces : nic.name => nic  if length(var.network_interfaces) > 0}
   name                = "${var.vm_name}-${each.value.name}"
   location            = var.location
   resource_group_name = var.resource_group_name
 
-  accelerated_networking_enabled = each.value.accelerated_networking_enabled
+  accelerated_networking_enabled = coalesce(each.value.accelerated_networking_enabled, false)
   dynamic "ip_configuration" {
     for_each = each.value.nic_ip_config
     content {
       
       name = ip_configuration.value.name
       subnet_id = var.subnet_id
-      private_ip_address_allocation = ip_configuration.value.private_ip_address_allocation != null ? ip_configuration.value.private_ip_address_allocation : "Dynamic"
+      private_ip_address_allocation = lookup(ip_configuration.value, "private_ip_address_allocation", "Dynamic") 
 
 
     }
