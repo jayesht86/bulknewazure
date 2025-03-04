491a3b31f21ef57bcee91f80dc592d04f73afdbf
jayesht86 <37490933+jayesht86@users.noreply.github.com>
2025-03-03 22:09:29 -0800

    Create main-nic.tf

---
+++ main-nic.tf
@@ -0,0 +1,20 @@
+
+resource "azurerm_network_interface" "nic" {
+  for_each = { for nic in var.network_interfaces : nic.name => nic }
+  name                = "${var.vm_name}-${each.value.name}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  accelerated_networking_enabled = each.value.accelerated_networking_enabled
+  dynamic "ip_configuration" {
+    for_each = each.value.nic_ip_config
+    content {
+      
+      name = ip_configuration.value.name
+      subnet_id = var.subnet_id
+      private_ip_address_allocation = ip_configuration.value.private_ip_address_allocation != null ? ip_configuration.value.private_ip_address_allocation : "Dynamic"
+
+
+    }
+  }
+}
