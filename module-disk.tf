6bbed85483a2bd7710a41be6fa188f1ba519bd76
jayesht86 <37490933+jayesht86@users.noreply.github.com>
2025-03-05 06:01:40 -0800

    Create module-disk.tf

---
+++ module-disk.tf
@@ -0,0 +1,26 @@
+module "compute-managed_disk" {
+  source = "git::https://dev.azure.com/msci-otw/tech-iac/_git/terraform-azurerm-msci-compute-managed_disk?ref=0.10"
+
+  #Subscription
+  subscription_id = var.subscription_id
+
+  #Resource Group
+  resource_group_object = data.azurerm_resource_group.existing_rg
+
+  #Tags
+  tags = local.tags
+
+  #Variables
+
+  #Global Variables used by all deployments
+  ##Timeouts
+#   managed_disk_timeout_create = var.managed_disk_timeout_create
+#   managed_disk_timeout_update = var.managed_disk_timeout_update
+#   managed_disk_timeout_read   = var.managed_disk_timeout_read
+#   managed_disk_timeout_delete = var.managed_disk_timeout_delete
+
+  #Disk List
+
+  managed_disk = var.managed_disk
+
+}
