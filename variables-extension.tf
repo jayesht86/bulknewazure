fc4cf8bf804d60ddd118a850b15e0e4baed160ac
jayesht86 <37490933+jayesht86@users.noreply.github.com>
2025-03-10 17:59:05 +0530

    Update variables-extension.tf

--- variables-extension.tf
+++ variables-extension.tf
@@ -4,21 +4,21 @@ variable "vm_extension" {
 
     REQUIRED
 
-    `vm_extension_name`      - (Required) The name of the virtual machine extension peering. Changing this forces a new resource to be created.
+    `vm_extension_name`      - (Required) The name of the virtual machine extension. Changing this forces a new resource to be created.
 
-    `vm_extension_vm_id`     - (Required) The ID of the Virtual Machine. Changing this forces a new resource to be created
+    `vm_extension_vm_id`     - (Required) The ID of the Virtual Machine. Changing this forces a new resource to be created.
 
-    `vm_extension_publisher` - (Required) The publisher of the extension, available publishers can be found by using the Azure CLI. Changing this forces a new resource to be created.
+    `vm_extension_publisher` - (Required) The publisher of the extension, available publishers can be found by using the Azure CLI.
 
     `vm_extension_type`      - (Required) The type of extension, available types for a publisher can be found using the Azure CLI.
 
     `vm_extension_version`   - (Required) Specifies the version of the extension to use, available versions can be found using the Azure CLI.
 
-    UPGRADE
+    UPGRADE OPTIONS
 
-    `vm_extension_minor_version_upgrade` - (Optional) Specifies if the platform deploys the latest minor version update to the `vm_extension_version` specified. Defaults to true.
+    `vm_extension_minor_version_upgrade` - (Optional) Enables automatic deployment of minor version updates. Defaults to true.
 
-    `vm_extension_auto_upgrade` - (Optional) Should the Extension be automatically updated whenever the Publisher releases a new version of this VM Extension? Defaults to false.
+    `vm_extension_auto_upgrade` - (Optional) Should the Extension be automatically updated whenever the Publisher releases a new version? Defaults to false.
 
     SETTINGS
 
@@ -30,94 +30,99 @@ variable "vm_extension" {
 
   EOT
   type = object({
-    #Required
-    vm_extension_name = string
-    #vm_extension_vm_id = string
-    vm_name = string
+    # Required
+    vm_extension_name      = string
+    vm_name                = string
     vm_extension_publisher = string
-    vm_extension_type = string
-    vm_extension_version = string
-    #Upgrade
-    vm_extension_minor_version_upgrade = optional(bool)
-    vm_extension_auto_upgrade = optional(bool)
-    #Settings
-    vm_extension_settings = optional(string)
-    vm_extension_protected_settings = optional(string)
-    vm_extension_failure_suppression_enabled = optional(bool)
-    #Timeouts
-    vm_extension_timeout_create = optional(string)
-    vm_extension_timeout_update = optional(string)
-    vm_extension_timeout_read = optional(string)
-    vm_extension_timeout_delete = optional(string)
-    #Extensions
-    ##Monitoring Agent Extension
-    linux_vm_extension_monitoring_enabled       = optional(bool)
+    vm_extension_type      = string
+    vm_extension_version   = string
+
+    # Upgrade
+    vm_extension_minor_version_upgrade = optional(bool, true)
+    vm_extension_auto_upgrade          = optional(bool, false)
+
+    # Settings
+    vm_extension_settings               = optional(string)
+    vm_extension_protected_settings     = optional(string)
+    vm_extension_failure_suppression_enabled = optional(bool, false)
+
+    # Timeout Configurations
+    vm_extension_timeout_create = optional(string, "15m")
+    vm_extension_timeout_update = optional(string, "15m")
+    vm_extension_timeout_read   = optional(string, "5m")
+    vm_extension_timeout_delete = optional(string, "15m")
+
+    # Extensions
+    ## Monitoring Agent Extension
+    linux_vm_extension_monitoring_enabled       = optional(bool, false)
     linux_vm_extension_monitoring_workspace_id  = optional(string)
     linux_vm_extension_monitoring_workspace_key = optional(string)
-    linux_vm_extension_monitoring_failure_suppression_enabled = optional(bool,false)
-    ##Custom Script Extension
-    linux_vm_extension_custom_script_enabled                     = optional(bool)
+    linux_vm_extension_monitoring_failure_suppression_enabled = optional(bool, false)
+
+    ## Custom Script Extension
+    linux_vm_extension_custom_script_enabled                     = optional(bool, false)
     linux_vm_extension_custom_script_blob_storage_account_name   = optional(string)
     linux_vm_extension_custom_script_blob_storage_container_name = optional(string)
     linux_vm_extension_custom_script_blob_storage_blob_name      = optional(string)
     linux_vm_extension_custom_script_blob_storage_account_key    = optional(string)
     linux_vm_extension_custom_script_parameters                  = optional(string)
-    linux_vm_extension_custom_script_failure_suppression_enabled = optional(bool,false)
-    ##Encryption Extension
-    linux_vm_extension_encryption_enabled                  = optional(bool)
+    linux_vm_extension_custom_script_failure_suppression_enabled = optional(bool, false)
+
+    ## Encryption Extension
+    linux_vm_extension_encryption_enabled                  = optional(bool, false)
     linux_vm_extension_encryption_key_vault_name           = optional(string)
     linux_vm_extension_encryption_key_vault_resource_group = optional(string)
     linux_vm_extension_encryption_key_vault_kek_name       = optional(string)
     linux_vm_extension_encryption_key_vault_kek_version    = optional(string)
-    linux_vm_extension_encryption_encrypt_all_disks        = optional(bool)
+    linux_vm_extension_encryption_encrypt_all_disks        = optional(bool, false)
     linux_vm_extension_encryption_operation                = optional(string)
-    linux_vm_extension_encryption_failure_suppression_enabled = optional(bool,false)
-    ##ADO Agent Extension
-    linux_vm_extension_ado_agent_enabled           = optional(bool)
+    linux_vm_extension_encryption_failure_suppression_enabled = optional(bool, false)
+
+    ## ADO Agent Extension
+    linux_vm_extension_ado_agent_enabled           = optional(bool, false)
     linux_vm_extension_ado_agent_name              = optional(string)
     linux_vm_extension_ado_agent_vsts_account_name = optional(string)
     linux_vm_extension_ado_agent_team_project      = optional(string)
     linux_vm_extension_ado_agent_deployment_group  = optional(string)
     linux_vm_extension_ado_agent_pat_token         = optional(string)
     linux_vm_extension_ado_agent_tags              = optional(string)
-    linux_vm_extension_ado_agent_failure_suppression_enabled = optional(bool,false)
-    ##AAD SSH Extension
-    linux_vm_extension_aad_ssh_enabled             = optional(bool)
-    linux_vm_extension_aadssh_failure_suppression_enabled = optional(bool,false)
+    linux_vm_extension_ado_agent_failure_suppression_enabled = optional(bool, false)
+
+    ## AAD SSH Extension
+    linux_vm_extension_aad_ssh_enabled                     = optional(bool, false)
+    linux_vm_extension_aadssh_failure_suppression_enabled = optional(bool, false)
   })
+
   default = {
-    vm_extension_name = "MonitoringAgentExtension"
-    #vm_extension_vm_id = "default_id"
-    vm_name = "eu1pcd109"
+    vm_extension_name      = "MonitoringAgentExtension"
+    vm_name                = "eu1pcd109"
     vm_extension_publisher = "Microsoft.EnterpriseCloud.Monitoring"
-    vm_extension_type = "OmsAgentForLinux"
-    vm_extension_version = "1.14"
+    vm_extension_type      = "OmsAgentForLinux"
+    vm_extension_version   = "1.14"
   }
 }
 
-#Global Variables
-#Timeouts
+# Global Variables - Timeout Settings
 variable "vm_extension_timeout_create" {
   description = "Specify timeout for create action. Defaults to 15 minutes."
   type        = string
   default     = "15m"
 }
+
 variable "vm_extension_timeout_update" {
   description = "Specify timeout for update action. Defaults to 15 minutes."
   type        = string
   default     = "15m"
 }
+
 variable "vm_extension_timeout_read" {
   description = "Specify timeout for read action. Defaults to 5 minutes."
   type        = string
   default     = "5m"
 }
+
 variable "vm_extension_timeout_delete" {
   description = "Specify timeout for delete action. Defaults to 15 minutes."
   type        = string
   default     = "15m"
 }
-
-
-
-
