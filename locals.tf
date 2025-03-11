e5ff2756b98178cc50f11d56c6c39db34e8b92a1
jayesht86 <37490933+jayesht86@users.noreply.github.com>
2025-03-11 04:14:37 -0700

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -13,15 +13,43 @@ locals {
         admin_password  = config.admin_password
         os_disk_size_gb = config.os_disk_size_gb
         zone            = zone
-        
-
+        linux_vm_extension_monitoring_enabled = config.linux_vm_extension_monitoring_enabled
+        vm_extension_failure_suppression_enabled = config.vm_extension_failure_suppression_enabled
+        linux_vm_extension_custom_script_failure_suppression_enabled = config.linux_vm_extension_custom_script_failure_suppression_enabled
+        linux_vm_extension_encryption_failure_suppression_enabled = config.linux_vm_extension_encryption_failure_suppression_enabled
+        linux_vm_extension_ado_agent_failure_suppression_enabled = config.linux_vm_extension_ado_agent_failure_suppression_enabled
+        linux_vm_extension_monitoring_workspace_id = config.linux_vm_extension_monitoring_workspace_id
+        linux_vm_extension_monitoring_workspace_id = config.linux_vm_extension_monitoring_workspace_id
+        linux_vm_extension_monitoring_workspace_key        = config.linux_vm_extension_monitoring_workspace_key
+        linux_vm_extension_custom_script_enabled           = config.linux_vm_extension_custom_script_enabled
+        linux_vm_extension_custom_script_blob_storage_container_name = config.linux_vm_extension_custom_script_blob_storage_container_name
+        linux_vm_extension_custom_script_blob_storage_blob_name    = config.linux_vm_extension_custom_script_blob_storage_blob_name
+        linux_vm_extension_custom_script_blob_storage_account_name = config.linux_vm_extension_custom_script_blob_storage_account_name
+        linux_vm_extension_custom_script_blob_storage_account_key  = config.linux_vm_extension_custom_script_blob_storage_account_key
+        linux_vm_extension_encryption_enabled                      = config.linux_vm_extension_encryption_enabled
+        linux_vm_extension_encryption_operation                    = config.linux_vm_extension_encryption_operation
+        linux_vm_extension_encryption_key_vault_name               = config.linux_vm_extension_encryption_operation
+        linux_vm_extension_encryption_key_vault_resource_group     = config.linux_vm_extension_encryption_operation
+        linux_vm_extension_encryption_encrypt_all_disks    = config.linux_vm_extension_encryption_encrypt_all_disks
+        linux_vm_extension_encryption_key_vault_resource_group   = config.linux_vm_extension_encryption_key_vault_resource_group
+        linux_vm_extension_encryption_key_vault_name      = config.linux_vm_extension_encryption_key_vault_name
+        linux_vm_extension_ado_agent_enabled               = config.linux_vm_extension_ado_agent_enabled
+        linux_vm_extension_ado_agent_vsts_account_name    = config.linux_vm_extension_ado_agent_vsts_account_name
+        linux_vm_extension_ado_agent_team_project        = config.linux_vm_extension_ado_agent_team_project
+        linux_vm_extension_ado_agent_deployment_group    = config.linux_vm_extension_ado_agent_deployment_group
+        linux_vm_extension_ado_agent_name               = config.linux_vm_extension_ado_agent_name
+        linux_vm_extension_ado_agent_tags              = config.linux_vm_extension_ado_agent_tags
+        linux_vm_extension_ado_agent_pat_token        =  config.linux_vm_extension_ado_agent_pat_token
+        linux_vm_extension_aad_ssh_enabled     = config.linux_vm_extension_aad_ssh_enabled
+        linux_vm_extension_aadssh_failure_suppression_enabled = config.linux_vm_extension_aadssh_failure_suppression_enabled
+        linux_vm_extension_monitoring_failure_suppression_enabled = config.linux_vm_extension_monitoring_failure_suppression_enabled
+        linux_vm_extension_encryption_key_vault_kek_version = config.linux_vm_extension_encryption_key_vault_kek_version
+        linux_vm_extension_encryption_key_vault_kek_name = config.linux_vm_extension_encryption_key_vault_kek_name
 
       }
     ]
-    ]
+  ]
   )
-
-
   effective_disk_count = var.managed_disk != null ? length(var.managed_disk.managed_disks_list) : 0
 
   vm_managed_disk_extended_list = flatten([
@@ -44,7 +72,7 @@ locals {
         managed_disk_sector_size = var.managed_disk.managed_disk_sector_size
         managed_disk_tier        =var.managed_disk.managed_disk_tier
         managed_disk_max_shares  = var.managed_disk.managed_disk_max_shares
-        managed_disk_zone        = var.managed_disk.managed_disk_zone
+        managed_disk_zone        = obj.zone  #var.managed_disk.managed_disk_zone
         #Ultra SSD Options
         managed_disk_iops_read_write = var.managed_disk.managed_disk_iops_read_write
         managed_disk_iops_read_only  = var.managed_disk.managed_disk_iops_read_only
@@ -72,22 +100,28 @@ locals {
 
 
   ])
-disk_attachments = var.managed_disk != null ? flatten([
-  for idx, v in coalesce(var.managed_disk.managed_disk_attachment_vm_name, []) : {
-    vm   = v
-    disk = format("ddsk-%02d-%s", idx + 1, v) # Ensuring exact match
-    lun  = idx # Assigning LUN based on index
-    cache = coalesce(var.managed_disk.managed_disk_attachment_cache, "None")
-    create_option = coalesce(var.managed_disk.managed_disk_attachment_create_option, "Attach")
-    write_accelerator = coalesce(var.managed_disk.managed_disk_attachment_write_accelerator_enabled, false)
-  }
-]) : []
 
-  disk_attachments2 = var.managed_disk != null ? { for o in local.disk_attachments : "${o.vm} + ${o.disk}" => o }  : {}
+vm_names = [for vm in local.vm_list : vm.name]
+ disk_attachments = var.managed_disk != null ? flatten([
+    for idx, vm in local.vm_names : [
+      for disk_idx, disk_size in var.managed_disk.managed_disks_list : {
+        vm   = vm
+        disk = format("ddsk-%02d-%s", disk_idx + 1, vm)
+        lun  = disk_idx 
+        cache = coalesce(var.managed_disk.managed_disk_attachment_cache, "None")
+        create_option = coalesce(var.managed_disk.managed_disk_attachment_create_option, "Attach")
+        write_accelerator = coalesce(var.managed_disk.managed_disk_attachment_write_accelerator_enabled, false)
+      }
+    ]
+  ]) : []
+  
+disk_attachments2 = var.managed_disk != null ? { for o in local.disk_attachments : "${o.vm} + ${o.disk}" => o }  : {}
+
+vm_lists = azurerm_linux_virtual_machine.vm
 
 }
 
-output "disk_list" {
-  value =  local.vm_managed_disk_extended_list
-}
+# output "disk_list" {
+#   value =  local.vm_managed_disk_extended_list
+# }
 
