dcbf3108a217ae5f98fe40bb5257ce0f54ade434
jayesht86 <37490933+jayesht86@users.noreply.github.com>
2025-03-13 13:05:50 +0530

    Create localsasg.tf

---
+++ localsasg.tf
@@ -0,0 +1,27 @@
+locals 
+
+locals {
+  # Generate ASG names dynamically based on VM name
+  asg_mapped = {
+    for vm in local.vm_list : vm.name => {
+      asg_name  = format("%s-asg", vm.name) # ASG name will be "vmname-asg"
+      nic_names = concat(
+        [vm.linux_vm_default_nic.nic_name], # Default NIC
+        [for nic in vm.linux_vm_additional_nic : nic.nic_name] # Additional NICs
+      )
+    }
+  }
+
+  # Flatten ASG associations for for_each loop
+  asg_attachment = flatten([
+    for vm_name, asg in local.asg_mapped : [
+      for nic in asg.nic_names : {
+        nic_name = nic
+        asg_name = asg.asg_name
+      }
+    ]
+  ])
+
+  # Convert to a map for easier lookup
+  asg_attachment_map = { for attachment in local.asg_attachment : "${attachment.nic_name}-${attachment.asg_name}" => attachment }
+}
