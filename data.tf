0d37d3814ec2371755eccea6fe237f9e60e96d12
jayesht86 <37490933+jayesht86@users.noreply.github.com>
2025-03-18 05:41:50 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -12,3 +12,9 @@ data "azurerm_subnet" "existing_subnet" {
   virtual_network_name = data.azurerm_virtual_network.existing_vnet.name
   resource_group_name  = data.azurerm_resource_group.existing_rg.name
 }
+data "azurerm_network_interface" "existing_nics" {
+  for_each            = toset(local.nic_names)
+  name                = each.value
+  resource_group_name = var.resource_group_name
+}
+
