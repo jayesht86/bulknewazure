cb860849e5957ce5cea131060737dc0693033f0a
jayesht86 <37490933+jayesht86@users.noreply.github.com>
2025-03-18 05:42:27 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -17,4 +17,15 @@ data "azurerm_network_interface" "existing_nics" {
   name                = each.value
   resource_group_name = var.resource_group_name
 }
+locals {
+  nic_names = flatten([
+    for vm in local.vm_list : concat(
+      [vm.linux_vm_default_nic.nic_name],
+      [for nic in vm.linux_vm_additional_nic : nic.nic_name]
+    )
+  ])
+
+  # Extract NIC IDs from data source
+  nic_list = { for name, nic in data.azurerm_network_interface.existing_nics : name => nic.id }
+}
 
