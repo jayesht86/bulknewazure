c6ab40a3a14c085f5bbe3f106f7f848a8f30c412
jayesht86 <37490933+jayesht86@users.noreply.github.com>
2025-03-19 11:27:10 -0700

    Update main-disk.tf

--- main-disk.tf
+++ main-disk.tf
@@ -1,12 +1,12 @@
 resource "azurerm_virtual_machine_data_disk_attachment" "disk_attachment" {
   for_each = local.disk_attachments2
 
-  managed_disk_id = try(module.compute-managed_disk.managed_disk_list[each.value.disk].id, null)
-  virtual_machine_id = try(one([for vm in azurerm_linux_virtual_machine.vm : vm.id if vm.name == each.value.vm]), null)
+  managed_disk_id           = try(module.compute-managed_disk.managed_disk_list[each.value.disk].id, null)
+  virtual_machine_id        = try(one([for vm in azurerm_linux_virtual_machine.vm : vm.id if vm.name == each.value.vm]), null)
   lun                       = each.value.lun
   caching                   = each.value.cache
   create_option             = each.value.create_option
   write_accelerator_enabled = each.value.write_accelerator
 
-  depends_on = [azurerm_linux_virtual_machine.vm]
+  
 }
