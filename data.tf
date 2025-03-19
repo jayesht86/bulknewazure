ae86f376fe77bc7a6007227144b4de4d82186507
jayesht86 <37490933+jayesht86@users.noreply.github.com>
2025-03-19 11:33:16 -0700

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,12 +1,13 @@
+# To retrive existing Resource Group
 data "azurerm_resource_group" "existing_rg" {
   name = var.resource_group_name
 }
-
+# To retrive existing Vnet
 data "azurerm_virtual_network" "existing_vnet" {
   name                = var.vnet_name
   resource_group_name = data.azurerm_resource_group.existing_rg.name
 }
-
+# To retrive existing subnet
 data "azurerm_subnet" "existing_subnet" {
   name                 = var.subnet_name
   virtual_network_name = data.azurerm_virtual_network.existing_vnet.name
@@ -16,44 +17,22 @@ data "azurerm_network_interface" "existing_nics" {
   for_each            = toset(local.nic_names)
   name                = each.value
   resource_group_name = var.resource_group_name
+  depends_on = [ module.nic ]
 }
-locals {
-  nic_names = flatten([
-    for vm in local.vm_list : concat(
-      [vm.linux_vm_default_nic.nic_name],
-      [for nic in vm.linux_vm_additional_nic : nic.nic_name]
-    )
-  ])
-
-  # Extract NIC IDs from data source
-  nic_list = { for name, nic in data.azurerm_network_interface.existing_nics : name => nic.id }
-}
-
-
-association 
-resource "azurerm_network_interface_application_security_group_association" "asg_association" {
-  for_each = local.asg_attachment_map
-
-  network_interface_id          = local.nic_list[each.value.nic_name] # ✅ Fetches NIC ID dynamically
-  application_security_group_id = module.network-asg.asg_list[each.value.asg_name] # ✅ Uses ASG IDs from module output
-
-
 data "azurerm_application_security_group" "existing_asgs" {
   for_each            = toset([for asg in var.asg_config : asg.asg_name])
   name                = each.value
   resource_group_name = var.resource_group_name
 depends_on = [module.network-asg]
 }
-
-
- asg_list = {
-    for asg in data.azurerm_application_security_group.existing_asgs :
-    asg.name => asg.id
-  }
-network_interface_id          = lookup(local.nic_list, each.value.nic_name, null) # ✅ Fetch NIC ID dynamically
-  application_security_group_id = lookup(local.asg_list, each.value.asg_name, null) # ✅ Fetch ASG ID dynamically
-}
-}
-
-
-
+# terraform {
+#   backend "azurerm" {
+#     #tenant_id            = "7a9376d4-7c43-480f-82ba-a090647f651d" #MSCI OFFICE 365
+#     subscription_id      = "59bab8f2-1319-472b-bb69-314d1b034ec0" #sandbox
+#     resource_group_name  = "rgrp-dva2-ic-terratest"               #East US 2 RGRP
+#     storage_account_name = "saccdva2icterratest003"
+#     container_name       = "tfstate-compute-linux-vm-bulk"
+#     key                  = "tfstate-compute-linux-vm-bulk.terraform.tfstate"
+#     snapshot             = true
+#   }
+# }
