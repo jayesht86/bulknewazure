5057a531df5f5c6ad53e902ec825a6b900771a64
jayesht86 <37490933+jayesht86@users.noreply.github.com>
2025-03-19 11:28:44 -0700

    Update module-asg.tf

--- module-asg.tf
+++ module-asg.tf
@@ -1,6 +1,9 @@
 module "network-asg" {
   source = "git::https://dev.azure.com/msci-otw/tech-iac/_git/terraform-azurerm-msci-network-asg?ref=0.3"
-  for_each = { for obj in local.vm_list : obj.name => obj }
+  #for_each = { for obj in local.vm_list : obj.name => obj }
+  #for_each = { for obj in local.asg_attachment : obj.name => obj}
+  for_each = local.asg_mapped
+  
   #Subscription
   subscription_id = var.subscription_id
 
@@ -8,8 +11,11 @@ module "network-asg" {
   resource_group_object = data.azurerm_resource_group.existing_rg
 
   #Tags
-  tags = local.tags
+  #tags = local.tags
+  tags                  = merge(each.value.asg_custom_tags != null ? each.value.asg_custom_tags : {}, var.tags)
 
   #ASG
-  asg = var.asg
+  asg              =  [each.value]#each.value.asg_name
+  #asg = var.asg
+
 }
